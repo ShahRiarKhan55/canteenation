15edb8dcd27a0046d2e6ff38bbd4c5fc2eefb165
ShahRiarKhan55 <siaabid1234@gmail.com>
2023-07-25 17:29:10 +0600

    25th July Updated

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require("./db");
+app.use(express.json());
+app.use(require("./router/authorization")); //linking the router file to be in use
+app.get("/", (req, res) => {
+  res.send(`hello this is backend`);
+});
+
+app.get("/student_login", (req, res) => {
+  res.send(`Hello this is from server attaching the token khi khi`);
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000");
+});
